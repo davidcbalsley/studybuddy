7aec1aa039584cc0813b26eac5b4c99f8911f04a
Ian Erickson <ierickson25@gmail.com>
2020-02-08 13:30:34 -0600

    updated index file with updated account creation and removal of placeholder index

--- submitForm.js
+++ submitForm.js
@@ -5,14 +5,58 @@ $(document).ready(function() {
     event.preventDefault();
 
     //Collecting input into variables
-    var firstName = $("#first-name").val();
-    var lastName = $("#last-name").val();
-    var linkedInURL = $("#linkedIn-url").val();
-    var learningTopic = $("#learn-topics").val();
+    var inputFirstName = $("#first-name").val();
+    var inputLastName = $("#last-name").val();
+    var inputEmail = $("#email").val();
+    var inputLinkedInURL = $("#linkedIn-url").val();
 
-    console.log(firstName);
-    console.log(lastName);
-    console.log(linkedInURL);
-    console.log(learningTopic);
+    //Topics are collected via the textbox and separated via topicSeparater function
+    var inputLearningTopics = $("#learn-topics").val();
+    inputLearningTopics = topicSeparator(inputLearningTopics);
+
+    var inputExpLevel = $(
+      "input[name=experience]:checked, #experience-level"
+    ).val();
+
+    //This variable will keep track of total number of StudyBuddies and increment when new profile is created
+    var userCount = 0;
+
+    console.log("------Input Tests------");
+    console.log(inputFirstName);
+    console.log(inputLastName);
+    console.log(inputEmail);
+    console.log(inputLinkedInURL);
+    console.log(inputLearningTopics);
+    console.log(inputExpLevel);
+    console.log(userCount);
+    console.log("_______________________");
+
+    createStudyBuddyObj();
   });
+
+  ///////////FUNCTIONS////////////////
+
+  //Takes the string of learning topics, separates at comma, and returns array of tags
+  function topicSeparator(tags) {
+    var tagsArray = tags.split(", ");
+
+    return tagsArray;
+  }
+
+  //Creates Study Buddy
+  function createStudyBuddyObj() {
+    //declares a new StudyBuddy Object
+    var studdyBuddy = {
+      id: userCount,
+      firstName: inputFirstName,
+      lastName: inputLastName,
+      email: inputEmail,
+      linkedIn: inputLinkedInURL,
+      learningTags: inputLearningTopics,
+      expLevel: inputExpLevel
+    };
+    userCount++;
+    //clear input fields function
+    //commit it to local storage
+  }
 });
