c5e151bd33cb469096e03074bb6478d8c4bd0e4b
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-02-08 09:38:20 -0600

    working on search

---
+++ search.js
@@ -0,0 +1,16 @@
+/* Okay. What does this need to do? */
+
+/* Given a coding concept, search users to find users who have that tag,
+   return some summary info about them 
+
+   Keep in mind that the Stack Exchange API only lets you search for tags 
+   by user -- need user id to search for any tags */
+
+// Okay. Start with tag and list of user ids
+// (Step 2 for app could be adding connected users, )
+
+// Look for Stack Overflow users who have a particular interest
+// listed as a tag
+// Return an array of users, include name, location, reputation, and
+// score for this particular interest, along with userid
+function getUsersWithInterest(interest)
