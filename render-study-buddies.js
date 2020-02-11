d511aa7b689b0801a79091d639eae30a4364754f
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-02-10 21:16:13 -0600

    getting study buddies from local storage and displaying them

--- render-study-buddies.js
+++ render-study-buddies.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
         // Get list of study buddies from local storage
         studyBuddies = JSON.parse(localStorage.getItem("study-buddies"));
-        console.log("studyBuddies = " + studyBuddies); // debug
+        // console.log("studyBuddies = " + studyBuddies); // debug
 
         if (studyBuddies) {
             for (var i = 0; i < studyBuddies.length; i++) {
@@ -22,28 +22,50 @@ $(document).ready(function() {
                 newLastNameTD = $("<td>");
                 newLastNameTD.text(studyBuddies[i].lastName);
 
+                // Email address -- put email address in table as mailto link to address
+                newEmailTD = $("<td>");
+                newEmailLink = $("<a>");
+                newEmailLink.text(studyBuddies[i].emailAddress);
+                newEmailLink.attr("href", "mailto:" + studyBuddies[i].emailAddress);
+                newEmailTD.append(newEmailLink);
+
+                // LinkedIn profile -- put profile in table as link to LinkedIn profile
+                var newLinkedInTD = $("<td>");
+                newLinkedInLink = $("<a>");
+                newLinkedInLink.text(studyBuddies[i].linkedInProfile);
+                newLinkedInLink.attr("href", studyBuddies[i].linkedInProfile);
+                newLinkedInTD.append(newLinkedInLink);
+
+                // Experience level
+                var newExperienceTD = $("<td>");
+                newExperienceTD.text(studyBuddies[i].experienceLevel);
+
+                // Interests
+                var newInterestsTD = $("<td>");
+                var currentBuddyInterests = null;
+                if (studyBuddies[i].interests) {
+
+                    // Create an unordered list of interests
+                    var newInterestsUL = $("<ul>");
+
+                    // Add each buddy's insterest as a line item to the unordered list
+                    currentBuddyInterests = studyBuddies[i].interests;
+                    for (var j = 0; j < currentBuddyInterests.length; j++) {
+                        var newInterestLI = $("<li>");
+                        newInterestLI.text(currentBuddyInterests[j]);
+                        newInterestsUL.append(newInterestLI);          
+                    }
+
+                    newInterestsTD.append(newInterestsUL);
+                }
+
                 // Append fields to new row
-                newRow.append(newFirstNameTD, newLastNameTD);
+                newRow.append(newFirstNameTD, newLastNameTD, newEmailTD, newLinkedInTD, newExperienceTD, newInterestsTD);
 
                 // Append fields to newRow
                 $("#study-buddy-table-body").append(newRow);
             }
         }
-
-        // For now, make sure I can add rows to tables using just hardcoded values
-        
-
-        /* newFirstNameTD = $("<td>")
-        // newFirstNameTD.text("David"); // works great -- debug
-        newRow.append(newFirstNameTD);
-        $("#study-buddy-table-body").append(newRow);
-
-        lastRow = $("#study-buddy-table-body").lastChild;
-        console.log("lastRow = " + lastRow);
-
-        // lastRow.append("<tr><td>Johnny</td><td>Slimjeans</td></tr>");  // Doesn't work -- lastRow undefined -- debug
-        // $("#study-buddy-table-body").append("<tr><td>Johnny</td><td>Slimjeans</td></tr>"); // Works -- debug
-        */
     }
 
     // Function, just for debugging purposes, to put at least one buddy in local storage
